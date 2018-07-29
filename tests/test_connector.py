6ea252ec127b379828a64d99da6d0c3296effe5f
Alexander Malev <aamalev@users.noreply.github.com>
2018-07-29 21:17:11 +0300

    Simple test

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,10 +1,3 @@
-from aioworkers.core.context import Context
 
-from aioworkers_mongo.base import Connector
-
-
-async def test_connect(loop, config):
-    context = Context({}, loop=loop)
-    mongo = Connector(config, context=context, loop=loop)
-    await mongo.init()
-    assert await mongo.list_database_names()
+async def test_connect(context):
+    assert await context.mongo.list_database_names()
