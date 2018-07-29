05fbadebd3583a464c6d40c598c6f741f426fbed
Alexander Malev <aamalev@users.noreply.github.com>
2018-07-29 21:10:47 +0300

    Context in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from aioworkers.core.config import Config
+from aioworkers.core.context import Context
 
 
 @pytest.fixture
 def config():
-    return Config(mongo={
-        'uri': 'mongodb://localhost:27017/',
-    })
+    return Config(
+        mongo={
+            'uri': 'mongodb://localhost:27017/',
+        }
+    )
+
+@pytest.fixture
+def context(config, loop):
+    return Context(config, loop=loop)
