7817b092ea5ac6bb773c070a9cf097bb40cf72a1
Alex <Alex@test>
2022-02-17 15:39:56 +0500

    #2: Add black support.

--- aioworkers_mongo/base.py
+++ aioworkers_mongo/base.py
@@ -3,22 +3,22 @@ import logging
 from aioworkers.core.base import AbstractConnector
 from motor.motor_asyncio import AsyncIOMotorClient
 
-logger = logging.getLogger('aioworkers_mongo')
+logger = logging.getLogger("aioworkers_mongo")
 
 
 class Connector(AbstractConnector):
-    default_mongo_uri = 'mongodb://localhost:27017/'
+    default_mongo_uri = "mongodb://localhost:27017/"
 
     # List of methods that will be bind to connector from AsyncIOMotorClient
     __bind_methods = (
-        'list_databases',
-        'list_database_names',
-        'server_info',
-        'start_session',
-        'end_session',
-        'drop_database',
-        'get_database',
-        'close',
+        "list_databases",
+        "list_database_names",
+        "server_info",
+        "start_session",
+        "end_session",
+        "drop_database",
+        "get_database",
+        "close",
     )
 
     def __init__(self, config=None, *, context=None, loop=None):
@@ -27,7 +27,7 @@ class Connector(AbstractConnector):
 
     async def init(self):
         await super().init()
-        uri = self.config.get('uri', self.default_mongo_uri)
+        uri = self.config.get("uri", self.default_mongo_uri)
         self._client = AsyncIOMotorClient(uri)
         for method_name in self.__bind_methods:
             f = getattr(self._client, method_name)
--- examples/log/worker.py
+++ examples/log/worker.py
@@ -2,7 +2,9 @@ from datetime import datetime
 
 
 async def run(worker, *args, **kwargs):
-    worker.logger.info('OK')
-    await worker.context.mongo.test.log.insert_one({
-        'created_at': datetime.utcnow(),
-    })
+    worker.logger.info("OK")
+    await worker.context.mongo.test.log.insert_one(
+        {
+            "created_at": datetime.utcnow(),
+        }
+    )
