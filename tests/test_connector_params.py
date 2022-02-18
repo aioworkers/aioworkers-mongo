c594661e11c3317391de8324e81642e82b15e739
Alex <Alex@test>
2022-02-18 16:14:21 +0500

    Support client parameters.

---
+++ tests/test_connector_params.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def config_yaml():
+    return """
+    mongo:
+      cls: aioworkers_mongo.base.Connector
+      client:
+        host: localhost
+        port: 27017
+    """
+
+
+async def test_connector(context, db_name):
+    await context.mongo.db.test.insert_one({"test": "test"})
